3af1d0be1b18021e91413304fec00a25bb4b0ab0
Louis Capitanchik <ljcapitanio@gmail.com>
2017-01-27 02:08:20 +0000

    Slight cleanup

--- eg.js
+++
@@ -1,8 +0,0 @@
-const Module = require('module')
-const stripBOM = require('strip-bom')
-const fs = require('fs-jetpack')
-
-Module._extensions['.js'] = function(module, filename) {
-	const file = fs.read(filename, 'utf-8')
-	return module._compile(stripBOM(file), filename)
-}
--- src/examples/cp.js
+++
@@ -1,4 +0,0 @@
-const {StandardParser} = require('./lib/parsers/Standard')
-const fs = require('fs-jetpack');
-const src = fs.read('./src/examples/simple.js');
-const ps = new StandardParser(src)
